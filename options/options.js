004e632c4e97414d8572a8ca745e820c29dfa259
Aline Tavares <aline@crowdbotics.com>
2023-03-31 12:07:03 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -2,7 +2,7 @@ export const globalOptions = {
   "name": "aline-032923-dev-73007",
   "url": "https://aline-032923-dev-73007.botics.co",
   "api": "https://aline-032923-dev-73007.botics.co/api/v1",
-  "initialRoute": "Untitled2"
+  "initialRoute": "Untitled3"
 };
 export const modulesOptions = {
   "@modules/app-menu": {
