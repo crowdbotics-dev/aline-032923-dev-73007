b69c3447fdd7b4855b2e7c2a3080c5c562ebafc7
Crowdbotics <engineering@crowdbotics.com>
2023-04-11 14:02:04 +0000

    Update store.

--- store/rapidAPICocktails/api.js
+++ store/rapidAPICocktails/api.js
@@ -1,12 +1,7 @@
 import axios from "axios"
-import { RAPIDAPI_COCKTAILS_TOKEN } from "react-native-dotenv"
 const rapidAPICocktails = axios.create({
   baseURL: "https://drinks-digital1.p.rapidapi.com/v1/",
-  headers: {
-    Accept: "application/json",
-    "Content-Type": "application/json",
-    Authorization: `Bearer ${RAPIDAPI_COCKTAILS_TOKEN}`
-  }
+  headers: { Accept: "application/json", "Content-Type": "application/json" }
 })
 function rapidapicocktails_get_cocktails_list(payload) {
   return rapidAPICocktails.get(`/cocktails`, {
